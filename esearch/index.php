75172c55e2824d379c5ff205171a9603ea942cc9
mblickensdorf <marco.blickensdorf@gmail.com>
2014-10-15 16:58:48 +0200

    hits in search are now filtered better

--- esearch/index.php
+++ esearch/index.php
@@ -5,9 +5,9 @@ require '../init.php';
 <h2>Search</h2>
 
 <form action="index.php" method="get">
-<table border='0'>
+<table border='0' align="center">
 	<tr>
-		<th>Query</th><th>Options</th><th>Search in</th>
+		<th>Query</th><th>Options</th>
 	</tr>
 	<tr valign="top" cellspacing="10" cellpadding="20">
 		<td>
@@ -19,12 +19,13 @@ require '../init.php';
 			<input type="radio" name="fuzzy" value="0" <?php if($_GET['fuzzy']=='0'){echo "checked";}?>>exact Search<br>
 			<input type="radio" name="fuzzy" value="1" <?php if($_GET['fuzzy']=='1' || $_GET['fuzzy']==""){echo "checked";}?>>fuzzy Search<br>
 		</td>
-		<td>
+<!--		<td>
 			<input type="checkbox" name="field" value="authors" checked=true>Authors<br>
 			<input type="checkbox" name="field" value="publications" checked=true>Publications<br>
 			<input type="checkbox" name="field" value="topics" checked=true>Topics<br>
 			<input type="checkbox" name="field" value="tags" checked=true>Tags<br>
 		</td>
+-->
 	</tr>
 </table>
 <input type="hidden" name="task" value="search">
@@ -54,7 +55,7 @@ switch ($_GET['task']) {
 		}
 
 		$searchParams['body']['sort'] = ['_score'];
-		$searchParams['body']['size'] = 100;
+		$searchParams['body']['size'] = 1000;
 		// the actual query. Results are stored in a PHP array
 		$res = $client->search($searchParams);
 		$hits = $res['hits']['hits'];
@@ -79,52 +80,82 @@ switch ($_GET['task']) {
 				array_push($pubs, $source['pub_id']);
 			}
 			if(array_key_exists('topic_id',$source)){
-				array_push($topics, $source['topic_id']);
-				array_push($topics_name, $source['name']);
+				array_push($topics, array($source['topic_id'], $source['name']));
 			}
 			if(array_key_exists('tag_id',$source)){
-				array_push($tags, $source['tag_id']);
-				array_push($tags_name, $source['tag']);
-			}
-			if(array_key_exists('tag_id',$source)){
-				$oldarray = $tags;
-				array_push($tags, $source['tag_id']);
-				$newarray = array_unique($tags);
-				if(sizeof($oldarray) == sizeof($newarray)){					//check if new added entry is a doubling
-					$tags = $newarray;
-					array_push($tags_name, $source['tag']);
-				}				
-
+					array_push($tags, array($source['tag_id'], $source['tag']));				
 			}
 			
 			if(array_key_exists('author_id',$source)){
-				$oldarray = $authors;
-				array_push($authors, $source['author_id']);
-				$newarray = array_unique($authors);
-				if(sizeof($oldarray) == sizeof($newarray)){					//check if new added entry is a doubling
-					$authors = $newarray;
-					array_push($authors_firstname, $source['firstname']);
-					array_push($authors_lastname, $source['surname']);	
-				}				
-
+				array_push($authors, array($source['author_id'], $source['firstname'], $source['surname']));			
 			}
 
 		}
 		
+//		$pubs = array_unique($pubs);
+//		$authors = array_unique($authors);
+//		$tags = array_unique($tags);
+//		$topics = array_unique($topics);
+		//erase doubles
+		$shortauthors = array();
+		for($i = 0; $i < sizeof($authors);$i++){
+			$comp = $authors[$i];
+			$isin = FALSE;
+			for($j = 0; $j < sizeof($shortauthors); $j++){
+				if(strcmp($shortauthors[$j][0],$comp[0])==0 && strcmp($shortauthors[$j][1],$comp[1])==0 && strcmp($shortauthors[$j][2],$comp[2])==0){
+					$isin = TRUE;
+				}
+			}
+			if(! $isin){
+				array_push($shortauthors, $comp);
+			}
+		}
+		$authors = $shortauthors;
+		
+		$shorttags = array();
+		for($i = 0; $i < sizeof($tags);$i++){
+			$comp = $tags[$i];
+			$isin = FALSE;
+			for($j = 0; $j < sizeof($shorttags); $j++){
+				if(strcmp($shorttags[$j][0],$comp[0])==0 && strcmp($shorttags[$j][1],$comp[1])==0){
+					$isin = TRUE;
+				}
+			}
+			if(! $isin){
+				array_push($shorttags, $comp);
+			}
+		}
+		$tags = $shorttags;
+		
+		$shorttopics = array();
+		for($i = 0; $i < sizeof($topics);$i++){
+			$comp = $topics[$i];
+			$isin = FALSE;
+			for($j = 0; $j < sizeof($shorttopics); $j++){
+				if(strcmp($shorttopics[$j][0],$comp[0])==0 && strcmp($shorttopics[$j][1],$comp[1])==0){
+					$isin = TRUE;
+				}
+			}
+			if(! $isin){
+				array_push($shorttopics, $comp);
+			}
+		}
+		$topics = $shorttopics;
 
+		//make output-strings from arrays
 		$topics_content = "<span style='font-size:larger;'>List contains <b>".sizeof($topics)." Topics</b></span>";
 		for($i = 0; $i < sizeof($topics);$i++){
-			$topics_content = $topics_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/topics/?task=showTopic&topic_id=".$topics[$i]."'>".$topics_name[$i]."</a>";
-		}
-		
+			$topics_content = $topics_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/topics/?task=showTopic&topic_id=".$topics[$i][0]."'>".$topics[$i][1]."</a>";
+		}	
 		$tags_content = "<span style='font-size:larger;'>List contains <b>".sizeof($tags)." Tags</b></span>";
 		for($i = 0; $i < sizeof($tags);$i++){
-			$tags_content = $tags_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/tags/?task=showTag&tag_id=".$tags[$i]."'>".$tags_name[$i]."</a>";
+			$tags_content = $tags_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/tags/?task=showTag&tag_id=".$tags[$i][0]."'>".$tags[$i][1]."</a>";
 		}
 		$authors_content = "<span style='font-size:larger;'>List contains <b>".sizeof($authors)." Authors</b></span>";
 		for($i = 0; $i < sizeof($authors);$i++){
-			$authors_content = $authors_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/authors/?task=showAuthor&author_id=".$authors[$i]."'>".$authors_firstname[$i]." ".$authors_lastname[$i]."</a>";
+			$authors_content = $authors_content . "<br><a href='".BIBLIOGRAPHIE_WEB_ROOT . "/authors/?task=showAuthor&author_id=".$authors[$i][0]."'>".$authors[$i][1]." ".$authors[$i][2]."</a>";
 		}
+		//print it all
 		echo "
 		<table style='box-shadow: 5px 5px 5px #AAAAAA;padding: 5px;    margin-left: auto;margin-right: auto;'><tr>
 		<td id='PubListBox' class='active_cell' onclick='showSearchPub()'>".sizeof($pubs)." Hits in Publications</td>
